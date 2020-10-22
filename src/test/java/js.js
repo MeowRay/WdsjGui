1c8c015d1eb698e3e229c70997b5af5aacb585e8
MeowRay <owo@miaomiaomiao.com>
2020-10-23 07:23:42 +0800

    modify test javascript

--- src/test/java/js.js
+++ src/test/java/js.js
@@ -1,19 +1,17 @@
-
 main();
 function main(){
-    var reason = "$reason";
     var servers = "$server".split(",");
     var result = false;
-    if (reason==="$reason"){
-        reason = "";
-    }
-    player.sendMessage(servers);
-    player.sendMessage("单价是: $price");
-    if (eco.take(player,$price,"POINT",null,reason)){
-        player.sendMessage("购买成功");
+    handler.sendMessage("单价是: $price reason: " + reason);
+    if (eco.take(handler.getName(),$price,"POINT",reason)){
+        title.sendTitle(handler, "§a§l购买成功");
         result = true;
     } else {
-        player.sendMessage("购买失败");
+        handler.spigot().sendMessage(Java.type("net.wdsj.servercore.message.ServerMessages$Component").TAKE_POINT_FAIL);
     }
+    if (doneClose){
+        guimanager.static.close(handler);
+    }
+
     return result;
 }
