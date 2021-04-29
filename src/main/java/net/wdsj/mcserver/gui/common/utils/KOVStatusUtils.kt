bc07608be991f56e3648ae0fa246f068a510ab07
MeowRay <owo@miaomiaomiao.com>
2021-04-29 14:59:28 +0800

    KOVDB Utils encap

--- src/main/java/net/wdsj/mcserver/gui/common/utils/KOVStatusUtils.kt
+++ src/main/java/net/wdsj/mcserver/gui/common/utils/KOVStatusUtils.kt
@@ -19,40 +19,61 @@ import kotlin.reflect.KClass
  * @date  2021/4/27 13:02
  * @version 1.0
  */
-object KOVStatusUtils {
+class KOVStatusUtils {
 
-    val TYPE_MAP = mutableMapOf<KClass<*>, ItemControl<*, *>>()
-    val ITEM_MAP = mutableMapOf<KOVColumnEntity<*>, ItemCommonBuilder>()
+    companion object{
+        val INSTANCE = KOVStatusUtils()
 
-    init {
-
-    TYPE_MAP[Boolean::class] = object : ItemControl<Any, Boolean> {
-            override fun exec(
-                guiitem: GuiItemCommon<*, *>,
-                builder: ItemCommonBuilder,
-                kov: KOVHandlerCache<*>,
-                kovEntity: KOVColumnEntity<Boolean>,
-                handler: Any,
-                value: Boolean
-            ) {
-                if (handler is Player) {
-                    builder.addLore(handler.getLangMessage("global.type.boolean.${value.toString()}"))
-                }else{
-                    builder.addLore(EnumLang.ZH_CN.getLocal("global.type.boolean.${value.toString()}", emptyMap()) ?: "$value")
-                }
-
-                val executor: GuiMenuItemExecutor<Any , Any>
-                executor = object : GuiMenuItemExecutor<Any , Any>{
-                    override fun execute(menu: GuiMenu<Any, Any>, handler: Any?) {
-                        kov.set(kovEntity, !kov.get(kovEntity, false))
+        init {
+            INSTANCE.registerTypeControl(Boolean::class,  object : ItemControl<Any, Boolean> {
+                override fun exec(
+                    guiitem: GuiItemCommon<*, *>,
+                    builder: ItemCommonBuilder,
+                    kov: KOVHandlerCache<*>,
+                    kovEntity: KOVColumnEntity<Boolean>,
+                    handler: Any,
+                    value: Boolean
+                ) {
+                    if (handler is Player) {
+                        builder.addLore(handler.getLangMessage("global.type.boolean.${value.toString()}"))
+                    }else{
+                        builder.addLore(EnumLang.ZH_CN.getLocal("global.type.boolean.${value.toString()}", emptyMap()) ?: "$value")
                     }
+
+                    val executor: GuiMenuItemExecutor<Any , Any>
+                    executor = object : GuiMenuItemExecutor<Any , Any>{
+                        override fun execute(menu: GuiMenu<Any, Any>, handler: Any?) {
+                            kov.set(kovEntity, !kov.get(kovEntity, false))
+                        }
+                    }
+                    (guiitem as GuiItemCommon<Any, Any>).addActionExecutor(InventoryAction.LEFT , executor)
                 }
-
-
-                (guiitem as GuiItemCommon<Any, Any>).addActionExecutor(InventoryAction.LEFT , executor)
             }
+            )
         }
+
+
     }
+
+    private val TYPE_MAP = mutableMapOf<KClass<*>, ItemControl<*, *>>()
+    private val ITEM_MAP = mutableMapOf<KOVColumnEntity<*>, ItemCommonBuilder>()
+
+
+    @Synchronized
+    fun  registerTypeControl(kClass: KClass<*> , control: ItemControl<*,*>){
+        TYPE_MAP[kClass] = control
+    }
+
+    @Synchronized
+    fun unregisterTypeControl(kClass: KClass<*>) = TYPE_MAP.remove(kClass)
+
+    @Synchronized
+    fun registerItem(entity: KOVColumnEntity<out Any> , builder: ItemCommonBuilder) { ITEM_MAP[entity] = builder}
+
+    @Synchronized
+    fun unregisterItem(entity: KOVColumnEntity<out Any>) { ITEM_MAP.remove(entity)}
+
+
 
 fun <H : Any , I:Any> getItemRender(handler : H , cache : KOVHandlerCache<*> ,entity : KOVColumnEntity<out Any>) : GuiItemBase<H, I> {
     val value = cache.get(entity, false)
